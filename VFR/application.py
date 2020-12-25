b74345cb52e4db7b1a045a76ee535139a578e1f0
Dmitry Molokhov <molokhovdmitry@gmail.com>
2020-12-25 15:50:40 +0300

    First commit

---
+++ VFR/application.py
@@ -0,0 +1,106 @@
+import os
+import functools
+
+from flask.globals import current_app
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+import face_recognition, cv2
+from werkzeug.utils import secure_filename
+
+from VFR.db import get_db
+from VFR.upload import allowed_file
+
+bp = Blueprint('application', __name__)
+
+@bp.route('/', methods=('GET', 'POST'))
+def find():
+    if request.method == 'POST':
+        db = get_db()
+
+        # Check if user selected a file
+        file = request.files['file']
+        if file.filename == '':
+            flash('No video file uploaded')
+            return redirect(request.url)
+
+        # Check if the filename is allowed
+        if not allowed_file(file.filename):
+            flash('Wrong file extension')
+            return redirect(request.url)
+
+        # Upload file
+        filename = secure_filename(file.filename)
+        path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        
+        """
+        if error is None:
+            # Video Face Recognition
+
+            # Check if the file is a video file
+
+            # Load the video (OpenCV)
+            video = cv2.VideoCapture(video)
+
+            # Get framerate (OpenCV)
+            FPS = video.get(cv2.CAP_PROP_FPS)
+            
+            # Get name
+            
+            # Get duration
+
+            # Get dimensions
+            
+            # Initialize a frame counter
+            i = 0
+
+            # Initialize arrays for a plot
+            timeArray = []
+            faceArray = []
+            
+            step = 5
+            # Iterate over frames
+            while True:
+                # Skip "FPS * step" frames
+                if (i % (FPS * step) != 0) or i != 0:
+                    for k in range(int((FPS * step) - 1)):
+                        i += 1
+                        if not video.grab():
+                            break
+                        continue
+
+                # Get frame
+                frame = video.read()
+                
+                # Count frames
+                i += 1
+
+                # Stop when no frames left
+                if not frame[0]:
+                    break
+
+                # Save second
+                second = int(i / FPS)
+                timeArray.append(second)
+
+                # Print second
+                print("Second:", second, "Frame:", i)
+
+                # Find faces
+                face_locations = face_recognition.face_locations(frame[1])
+
+                faceAmount = len(face_locations)
+
+                # Save faces
+                faceArray.append(faceAmount)
+            return render_template('plot.html', FPS=FPS, step=step, i=i)
+        """
+        
+        # Delete file
+        os.remove(path)
+
+    
+    return render_template('index.html')
---
+++ VFR/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS videos;
+DROP TABLE IF EXISTS faces;
+
+CREATE TABLE videos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    'name' TEXT NOT NULL,
+    duration INTEGER NOT NULL,
+    FPS INTEGER NOT NULL,
+    dimensions TEXT NOT NULL
+);
+
+CREATE TABLE frames (
+    video_id INTEGER NOT NULL,
+    second INTEGER NOT NULL,
+    frame INTEGER NOT NULL,
+    faces INTEGER NOT NULL,
+    FOREIGN KEY (video_id) REFERENCES videos(id)
+);
