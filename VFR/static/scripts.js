62ebcb2bfca47090168373aab348f4483d9961df
Dmitry Molokhov <molokhovdmitry@gmail.com>
2020-12-29 17:31:29 +0300

    About page update

--- VFR/static/scripts.js
+++ VFR/static/scripts.js
@@ -4,7 +4,7 @@ function showFileName() {
     document.getElementById('inputField').innerHTML = name.files.item(0).name;
   }
 
-// Shows a loading animation when form is submitted
+// Shows loading animation when form is submitted
 function loadingAnimation() {
 document.getElementById('loader').style.display = "block";
 }
